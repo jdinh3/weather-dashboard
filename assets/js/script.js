b5270ee5c7e9d93675126ed41e98cf82cbb11479
Jenny Dinh <jenny.p.dinh@gmail.com>
2021-03-29 18:54:46 -0500

    hardcoded layout and added functions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
+var searchForm = $("#search-form");
+var searchTermEl = $("#search-term");
 
+searchForm.on("submit", function (event) {
+  event.preventDefault();
+  var searchTerm = searchTermEl.val();
+  console.log(searchTerm);
+
+  var apiKey = "e919f026e83901dae380556be5bccfeb";
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    searchTerm +
+    "&units=imperial&appid=" +
+    apiKey;
+
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+});
